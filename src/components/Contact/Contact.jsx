1b148b9c02af574da990277eac56d19f66b99b47
asib ahmed <ahmedasib1@gmail.com>
2023-12-28 13:56:29 +0600

    add map

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <div>
             <p>map</p>
             <div className="max-w-1/2 my-6" >
-            <iframe className="w-full rounded border-4 border-[#800000] " src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3656.130843263617!2d58.545299075331265!3d23.599639978773574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDM1JzU4LjciTiA1OMKwMzInNTIuNCJF!5e0!3m2!1sen!2sbd!4v1703740059554!5m2!1sen!2sbd" width="1280" height="600"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe className="w-full rounded border-4 border-[#800000]" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3656.130843263617!2d58.545299075331265!3d23.599639978773574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDM1JzU4LjciTiA1OMKwMzInNTIuNCJF!5e0!3m2!1sen!2sbd!4v1703740059554!5m2!1sen!2sbd" width="800" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     );
