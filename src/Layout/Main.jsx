73c3a1fa274937a0a573d63db320bc25c827c195
asib ahmed <ahmedasib1@gmail.com>
2023-12-31 00:22:45 +0600

    fixed some issue

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,13 +3,16 @@ import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
 const Main = () => {
-    let copy = String.fromCodePoint(0x00A9);
+    const copy = String.fromCodePoint(0x00A9);
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
-            <p className="text-center text-xs rounded-sm font-semibold mt-1 bg-[#F2BA49]">copyright{copy} 2024 || Develop by <a className='font-mono font-bold hover:font-extrabold hover:underline text-[#fd5252]' href="https://wa.me/+8801753249719" target="_blank" rel="noreferrer">ASIB AHMED</a></p>
+            <div className="flex justify-between text-xs rounded-sm font-semibold mt-1 px-4">
+                <p >copyright{copy} 2024</p>
+                <p> Develop by <a className='font-mono font-bold hover:font-extrabold hover:underline text-[#C32148]' href="https://wa.me/+8801753249719" target="_blank" rel="noreferrer">ASIB AHMED</a></p>
+            </div>
         </div>
     );
 };
