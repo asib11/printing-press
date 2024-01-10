9361e0f72e213bab7126461f28899184cc766a49
asib ahmed <ahmedasib1@gmail.com>
2024-01-10 15:50:38 +0600

    add business card design

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -44,10 +44,14 @@ import BusinessImage from './BusinessImage'
 const BusinessCard = () => {
     const images = [img1,img2,img3,img4,img5, img6, img7,img8, img9, img10, img11,img12,img13,img14,img15,img16, img17, img18,img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33,img34, img35, img36,img37, img38, img39, img40, img41, img42 ]
     return (
-        <div className=' grid grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+        <div >
+            <h1 className='ml-4 text-5xl font-bold text-[#800000]'>Business Card</h1>
+            <div className=' grid grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
             {
                 images.map((image,i) => <BusinessImage key={i} image = {image}></BusinessImage>)
             }
+            </div>
+            
         </div>
     );
 };
