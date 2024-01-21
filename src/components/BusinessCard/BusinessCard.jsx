a9ffddb7ed98b9b9cea1783341021456cc80706d
asib ahmed <ahmedasib1@gmail.com>
2024-01-21 11:23:50 +0600

    add image

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -1,14 +1,14 @@
-import sp1 from '/services/01 Complete/Business Card/New folder/Circle-One_Business_CardsWeb_5_1.jpg'
-import sp2 from '/services/01 Complete/Business Card/New folder/Coastal-Shipping-Links-India-Company-Visiting-Card-Design.jpg'
-import sp3 from '/services/01 Complete/Business Card/New folder/Dr-Afzal-ENT-Care-Visiting-Card-Presentation.jpg'
-import sp4 from '/services/01 Complete/Business Card/New folder/Dropbox-Entertainments-Visiting-Card-Design-by-ZeroBulb-1536x864.jpg'
-import sp5 from '/services/01 Complete/Business Card/New folder/DTALE-Interior-Designing-Company-Kerala-Branding-Designing.jpg'
-import sp6 from '/services/01 Complete/Business Card/New folder/Fourset-Fitness-and-GYM-Health-Center-Logo-Design-ZeroBulb-Dubai.jpg'
-import sp7 from '/services/01 Complete/Business Card/New folder/Gramya-Ayurveda-Hospital-Professional-Logo-Design-Kerala.jpg'
-import sp8 from '/services/01 Complete/Business Card/New folder/Hotel-Caesar-Palace-India-Visiting-Card-Design.jpg'
-import sp9 from '/services/01 Complete/Business Card/New folder/ManasVR-Lab-Company-Logo-Visiting-Card-Design.jpg'
-import sp10 from '/services/01 Complete/Business Card/New folder/Opus-Penta-Visiting-Card-Design-Presentation.jpg'
-import sp11 from '/services/01 Complete/Business Card/New folder/Thoiba-Logistics-Dubai-Branding-Visiting-Card.jpg'
+// import sp1 from '/services/01 Complete/Business Card/New folder/Circle-One_Business_CardsWeb_5_1.jpg'
+// import sp2 from '/services/01 Complete/Business Card/New folder/Coastal-Shipping-Links-India-Company-Visiting-Card-Design.jpg'
+// import sp3 from '/services/01 Complete/Business Card/New folder/Dr-Afzal-ENT-Care-Visiting-Card-Presentation.jpg'
+// import sp4 from '/services/01 Complete/Business Card/New folder/Dropbox-Entertainments-Visiting-Card-Design-by-ZeroBulb-1536x864.jpg'
+// import sp5 from '/services/01 Complete/Business Card/New folder/DTALE-Interior-Designing-Company-Kerala-Branding-Designing.jpg'
+// import sp6 from '/services/01 Complete/Business Card/New folder/Fourset-Fitness-and-GYM-Health-Center-Logo-Design-ZeroBulb-Dubai.jpg'
+// import sp7 from '/services/01 Complete/Business Card/New folder/Gramya-Ayurveda-Hospital-Professional-Logo-Design-Kerala.jpg'
+// import sp8 from '/services/01 Complete/Business Card/New folder/Hotel-Caesar-Palace-India-Visiting-Card-Design.jpg'
+// import sp9 from '/services/01 Complete/Business Card/New folder/ManasVR-Lab-Company-Logo-Visiting-Card-Design.jpg'
+// import sp10 from '/services/01 Complete/Business Card/New folder/Opus-Penta-Visiting-Card-Design-Presentation.jpg'
+// import sp11 from '/services/01 Complete/Business Card/New folder/Thoiba-Logistics-Dubai-Branding-Visiting-Card.jpg'
 import img1 from '/services/01 Complete/Business Card/BC_1.jpg'
 import img2 from '/services/01 Complete/Business Card/BC_2.jpg'
 import img3 from '/services/01 Complete/Business Card/BC_3.jpg'
@@ -53,7 +53,7 @@ import img41 from '/services/01 Complete/Business Card/BC_41.jpg'
 import img42 from '/services/01 Complete/Business Card/BC_42.jpg'
 import BusinessImage from './BusinessImage'
 const BusinessCard = () => {
-    const images = [sp1,sp2,sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10, sp11, img1,img2,img3,img4,img5, img6, img7,img8, img9, img10, img11,img12,img13,img14,img15,img16, img17, img18,img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33,img34, img35, img36,img37, img38, img39, img40, img41, img42 ]
+    const images = [/*sp1,sp2,sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10, sp11,*/ img1,img2,img3,img4,img5, img6, img7,img8, img9, img10, img11,img12,img13,img14,img15,img16, img17, img18,img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33,img34, img35, img36,img37, img38, img39, img40, img41, img42 ]
     return (
         <div >
             <h1 className='ml-4 text-5xl font-bold text-[#800000]'>Business Card</h1>
