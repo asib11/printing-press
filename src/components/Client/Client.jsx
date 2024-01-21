05095d8b973dbdcbd96f66c0ef09d9eb108923a3
asib ahmed <ahmedasib1@gmail.com>
2024-01-21 14:02:27 +0600

    final work

--- src/components/Client/Client.jsx
+++ src/components/Client/Client.jsx
@@ -6,7 +6,7 @@ const Client = () => {
             <div className='flex flex-col justify-center mb-6'>
                 <div className='text-center mb-6'>
                     <h3 className='text-xl max-sm:text-center font-bold text-[#800000]'>Clients</h3>
-                    <h1 className="text-5xl font-bold">Our Big Client</h1>
+                    <h1 className="text-5xl font-bold">Our Some Clients</h1>
                 </div>
                 <img src={client} alt="" />
             </div>
--- src/components/Gifting/Gifting.jsx
+++ src/components/Gifting/Gifting.jsx
@@ -2,9 +2,12 @@ import GiftingImage from "./GiftingImage";
 import img1 from '/services/Mug Design/Al-Zaabi-Group-Abu-Dhabi-Logo-Design-1536x864.jpg'
 import img2 from '/services/Mug Design/Eorga-Organic-Company-Bangalore-Logo-Design.jpg'
 import img3 from '/services/Mug Design/ima.jpg'
+import img4 from '/services/Mug Design/img1.jpg'
+import img5 from '/services/Mug Design/img2.jpg'
+import img6 from '/services/Mug Design/img3.jpg'
 
 const Gifting = () => {
-    const images = [img3,img1,img2]
+    const images = [img3,img1,img2,img4,img5,img6]
     return (
         <div>
             <h1 className='ml-4 text-5xl font-bold text-[#800000]'>Gifting</h1>
